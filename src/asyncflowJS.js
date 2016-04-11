e80c75a4f0e38a87eb99d9381f53e1b099c98495
zwb@gli.cn <zwb@gli.cn>
2016-04-11 16:31:07 +0800

    事件集合化，将串行操作改变为并行操作

--- src/asyncflowJS.js
+++ src/asyncflowJS.js
@@ -44,6 +44,17 @@
         }
     }
 
+    function unique (array) {
+        var iteratee = each(1);
+        var result = [];
+
+        iteratee(array, function (value) {
+            result.indexOf(value) < 0 && result.push(value);
+        });
+
+        return result;
+    }
+
     /**
      * 对事件按照场景分别处理，转成单个事件名
      *
@@ -72,6 +83,51 @@
         return true;
     }
 
+    function assign (isOnce) {
+        typeof isOnce !== 'undefined' || (isOnce = true);
+
+        return function () {
+            var args = slice.call(arguments, 0);
+            var eventList = unique(args.slice(0, -2));
+            var callback = args[args.length - 2];
+            var iteratee = each();
+            var count = 0;
+            var result = {};
+
+            // 运用aop技术，在调用真实的callback的时候，先记录绑定的事件的调用次数和每个事件触发的数据，用于判断绑定的事件是否都触发完
+            var onCallback = (function (evtName) {
+                this[isOnce ? 'once' : 'on'](evtName, function (data) {
+                    (result[evtName] || (result[evtName] = {}))['data'] = data;
+                    count++;
+                });
+            }).bind(this);
+
+            // 定义global事件处理程序，判断当前集合事件是否执行完成
+            var onGlobalCallback = (function (evtName) {
+                var sortedResult = [], i, length;
+
+                // 判断所有事件是否调用完成
+                if (count < eventList.length) return;
+
+                for (i = 0, length = eventList.length; i < length; i++) {
+                    sortedResult.push(result[eventList[i]].data);
+                }
+
+                isOnce && this.offGlobalEvent(onGlobalCallback);
+                callback.apply(null, sortedResult);
+            }).bind(this);
+
+            iteratee(eventList, function (evtName, index) {
+                onCallback(evtName, index);
+            });
+
+            // 绑定global事件
+            this.onGlobalEvent(onGlobalCallback);
+
+            return this;
+        };
+    }
+
     function AsyncFlow () {
         if (!(this instanceof AsyncFlow)) {
             return new AsyncFlow();
@@ -86,13 +142,13 @@
 
     extend(AsyncFlow.prototype, {
 
-        /*
-        * 绑定事件
-        *
-        * @param {String|Array} eventName 单个或者多个事件名
-        * @param {Function} callback 事件处理程序
-        * @param {Object} context 上下文对象
-        * */
+        /**
+         * 事件绑定(可执行多次)
+         *
+         * @param {String|Array} eventName 单个或者多个事件名
+         * @param {Function} callback 事件处理程序
+         * @param {Object} context 上下文对象
+         */
         on: function (eventName, callback, context) {
             if (!eventName || !callback || !generalEventTo(this, 'on', eventName, [callback, context])) return this;
 
@@ -105,6 +161,9 @@
             return this;
         },
 
+        /**
+         * 事件绑定(只执行一次)
+         */
         once: function (eventName, callback, context) {
             if (!eventName || !callback || !generalEventTo(this, 'once', eventName, [callback, context])) return this;
 
@@ -116,6 +175,9 @@
             return this.on(eventName, onlyOnCallback, context);
         },
 
+        /**
+         * 删除事件绑定
+         */
         off: function (eventName) {
             var eventList, callbacks, currentCallbacks, result;
 
@@ -138,6 +200,9 @@
             return this;
         },
 
+        /**
+         * 触发事件绑定
+         */
         emit: function (eventName) {
             var args, currentCallbacks, sign, evtName;
 
@@ -156,6 +221,37 @@
             }
 
             return this;
+        },
+
+        onGlobalEvent: function (callback) {
+            this.on(GLOBAL_EVENT, callback);
+        },
+
+        offGlobalEvent: function (callback) {
+            this.off(GLOBAL_EVENT, callback);
+        },
+
+        all: assign(true),
+
+        tail: assign(false),
+
+        after: function (eventName, length, callback) {
+            var count = 0;
+            var result = [];
+            var onGlobalCallback = (function (evtName, data) {
+                if (evtName === eventName) {
+                    count++;
+
+                    if (count <= length) {
+                        result.push(data);
+                        return;
+                    }
+
+                    callback.apply(null, [result]);
+                }
+            }).bind(this);
+
+            this.onGlobalEvent(onGlobalCallback);
         }
     });
 
